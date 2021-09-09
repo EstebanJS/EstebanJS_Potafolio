b0d2f3c6aa857b9bb5cbfcda1c1f564b53b9e92e
Esteban Jimenez <yeisson310@hotmail.com>
2021-09-09 09:55:02 -0500

    Upload others proyects

--- src/components/Section/Projects.js
+++ src/components/Section/Projects.js
@@ -4,19 +4,32 @@ import './projets.css'
 // IMAGES
 import QrTools from '../../images/QrTools.png'
 import GIFNET from '../../images/GIFNET.png'
+import Egep from '../../images/Egep.png'
+import OverLabLanding from '../../images/OverLabLanding.png'
 
 const Projects = () => {
     const dataProjects = [
+        {
+            title:"Landing page OverLab",
+            link:"https://overlab-landing-page-lte1ki9m1-estebanjs.vercel.app/",
+            img:OverLabLanding
+        },
         {
             title:"QrTools",
             link:"https://qr-tools-vue.estebanjs.vercel.app/",
             img:QrTools
         },
         {
+            title:"Egep Consultoria",
+            link:"https://egep-consultores.vercel.app",
+            img:Egep
+        },
+        {
             title:"Gitnet",
             link:"https://gitnet-estebanjs.vercel.app/",
             img:GIFNET
         }
+
     ]
     return (
         <div className="content-projects">
@@ -26,7 +39,6 @@ const Projects = () => {
                         <ItemProyect key={item.title} title={item.title} link={item.link} img={item.img}/>
                     ))
                 }
-                
             </div>
         </div>
     )
